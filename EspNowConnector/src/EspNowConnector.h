804e01d65d925f1828bac7cf006d74f658e8b181
amavr <albert.mavrin@gmail.com>
2023-02-19 18:37:36 +0300

    Ext funcs

--- EspNowConnector/src/EspNowConnector.h
+++ EspNowConnector/src/EspNowConnector.h
@@ -9,7 +9,6 @@
 #include <ATools.h>
 #include <espnow.h>
 
-
 class EspNowConnector
 {
 private:
@@ -18,24 +17,56 @@ public:
     {
         WiFi.mode(WIFI_STA);
         bool res = esp_now_init() == 0;
-        esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+        Serial.printf("esp_now_init: %d\n", res);
+        esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
         return res;
     }
 
     void setReceiveCallback(esp_now_recv_cb_t onMsg)
     {
-        esp_now_register_recv_cb(onMsg);
+        int res = esp_now_register_recv_cb(onMsg);
+        Serial.printf("esp_now_register_recv_cb: %d\n", res);
     }
 
-    void send(const char *macAddr, const char *data)
+    void setSendCallback(esp_now_send_cb_t onSent)
+    {
+        int res = esp_now_register_send_cb(onSent);
+        Serial.printf("esp_now_register_send_cb: %d\n", res);
+    }
+
+    void pair(uint8_t *mac)
+    {
+        if (!esp_now_is_peer_exist(mac))
+        {
+            esp_now_add_peer(mac, ESP_NOW_ROLE_COMBO, 0, NULL, 0);
+        }
+    }
+
+    void pair(const char *mac)
+    {
+        uint8_t bytes[6];
+        ATools::macToBytes(mac, bytes);
+        pair(bytes);
+    }
+
+    void send(const char *macAddr, const char *data, bool makePair = false)
     {
         uint8_t mac[6];
         ATools::macToBytes(macAddr, mac);
 
-        // esp_now_add_peer(bytes, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
-        // esp_now_peer_info_t peerInfo;
+        if (makePair)
+        {
+            pair(macAddr);
+        }
 
-        Serial.printf("ADDR:%s\tMSG:%s\n", macAddr, data);
-        esp_now_send(mac, (uint8_t *)data, strlen(data));
+        int len = strlen(data);
+        // uint8_t *buf = (uint8_t *)malloc(len);
+        // memcpy(buf, data, len);
+        // buf[len] = 0;
+
+        Serial.printf("%s <- [%s]\n", macAddr, data);
+        esp_now_send(mac, (uint8_t *)data, len);
+
+        // free(buf);
     }
 };
--- EventBroker/src/TopicList.h
+++ EventBroker/src/TopicList.h
@@ -5,7 +5,7 @@
 
 struct Topic
 {
-    const char *name;
+    char *name;
     char *data = NULL;
     SubList *subscribers;
     Topic *next;
@@ -27,6 +27,22 @@ public:
     int Count()
     {
         return count;
+    }
+
+    void clear()
+    {
+        Topic *curr = head;
+        while (curr != NULL)
+        {
+            curr = curr->next;
+
+            head->subscribers->clear();
+            free(head->name);
+            free(head->subscribers);
+            free(head);
+            head = curr;
+        }
+        count = 0;
     }
 
     Topic *find(const char *topicName)
@@ -57,7 +73,8 @@ public:
             count++;
             // создание нового узла
             topic = new Topic;
-            topic->name = topicName;
+            topic->name = (char *)malloc(strlen(topicName) + 1);
+            strcpy(topic->name, topicName);
             topic->subscribers = new SubList;
             topic->next = NULL;
 
@@ -68,7 +85,7 @@ public:
                 curr = curr->next;
             }
             // нет предпоследнего элемента - список пуст
-            if(prev == NULL)
+            if (prev == NULL)
             {
                 head = topic;
             }
@@ -80,6 +97,43 @@ public:
             }
         }
         return topic;
+    }
+
+    int getSize()
+    {
+        int len = 0;
+        Topic *curr = head;
+        while (curr != NULL)
+        {
+            // сохранять надо только если есть подписчики
+            if (curr->subscribers->Count() > 0)
+            {
+                len += strlen(curr->name) + 1 + curr->subscribers->getSize();
+            }
+            curr = curr->next;
+        }
+        return len;
+    }
+
+    // заполнить подготовленный снаружи буффер
+    void fillTo(char *buf, char separator = '|')
+    {
+        int len = 0;
+        Topic *curr = head;
+        while (curr != NULL)
+        {
+            // сохранять надо только если есть подписчики
+            if (curr->subscribers->Count() > 0)
+            {
+                strcpy(buf + len, curr->name);
+                len += strlen(curr->name);
+                buf[len++] = separator;
+                len += curr->subscribers->fillTo(buf + len);
+                buf[len++] = '\n';
+            }
+            curr = curr->next;
+        }
+        // buf[len - 1] = '\0';
     }
 
     void forEach(void (*callback)(Topic *topic))
